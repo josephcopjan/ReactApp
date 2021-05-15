6651f44c190cd9d1a32b86b914d99087d833e3b5
jozef.copjan <jozef.copjan@sk.ibm.com>
2021-05-15 22:37:52 +0200

    master push

--- backend/src/main/java/com/in28minutes/springboot/Test.java
+++ backend/src/main/java/com/in28minutes/springboot/Test.java
@@ -9,6 +9,7 @@ public class Test {
 
         try{
            // aaa.equals("");
+            String ccc = null;
             throw new RuntimeException("text");
         } catch (Exception e) {
             int maxLength = e.getMessage().length();
